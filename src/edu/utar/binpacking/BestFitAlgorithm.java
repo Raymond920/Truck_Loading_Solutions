7efb58bccc4ff5441d18aa9bf632f9c4bc972e7d
Jade1028 <weijie2828@gmail.com>
2025-04-24 18:57:06 +0800

    Best Fit Algo

--- src/edu/utar/binpacking/BestFitAlgorithm.java
+++ src/edu/utar/binpacking/BestFitAlgorithm.java
@@ -1,13 +1,41 @@
 package edu.utar.binpacking;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class BestFitAlgorithm implements BinPackingAlgorithm {
 
-	@Override
-	public List<Truck> pack(List<Parcel> parcels, double loadLimit) {
-		// TODO Auto-generated method stubxx
-		return null;
-	}
+    @Override
+    public List<Truck> pack(List<Parcel> parcels, double loadLimit) {
+        List<Truck> truckList = new ArrayList<>();
+        truckList.add(new Truck(loadLimit));
 
+        for (Parcel parcel : parcels) {
+            int bestFitIndex = -1;
+            double minRemaining = Double.MAX_VALUE;
+
+            // Try to find the best-fit truck
+            for (int i = 0; i < truckList.size(); i++) {
+                Truck truck = truckList.get(i);
+                double remaining = truck.getRemainingLoad();
+
+                if (parcel.getWeight() <= remaining && (remaining - parcel.getWeight() < minRemaining)) {
+                    bestFitIndex = i;
+                    minRemaining = remaining - parcel.getWeight();
+                }
+            }
+
+            // If found a truck that fits best
+            if (bestFitIndex != -1) {
+                truckList.get(bestFitIndex).addParcel(parcel);
+            } else {
+                // No suitable truck found, create a new one
+                Truck newTruck = new Truck(loadLimit);
+                newTruck.addParcel(parcel);
+                truckList.add(newTruck);
+            }
+        }
+
+        return truckList;
+    }
 }
